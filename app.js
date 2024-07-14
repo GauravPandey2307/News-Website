172e288657266ceeba91927da3c246b27fd03f96
GauravPandey2307 <83923228+GauravPandey2307@users.noreply.github.com>
2024-07-15 01:32:03 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
+const cookieParser = require('cookie-parser');
 const express = require('express')
 const app=express()
 const port = process.env.PORT||3000;
 //const bodyParser = require('body-parser');
 const moment = require('moment')
+const mongoose = require("mongoose");
 app.locals.moment = moment;
-
+const connector= ()=>{
+    try{
+    mongoose.connect("mongodb://localhost:27017/newswebsite").then(()=>{console.log("database connected")}).catch((error)=>{console.log(error)});
+    app.listen(port,()=>{console.log("Server is running")});
+    }
+    catch(error)
+    {
+        console.log(error+"error occurred");
+    }
+}
+connector();
 // template engine  
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
 app.use(express.static('public'))
 app.set('view engine','ejs')
 
 app.use(express.urlencoded({ extended: true }));
-app.use('/',require('./routes/news'))
+app.use('/',require('./routes/news'));
 
-app.set('views','./views')
 
-app.listen(port,()=> console.log("started"))
